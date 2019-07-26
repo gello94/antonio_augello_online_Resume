6da134f7e02adc56b1a06e77f4eb4d986bbd037c
Ubuntu <ubuntu@ip-172-31-25-142.us-east-2.compute.internal>
2019-07-26 12:37:59 +0000

    initial commit

---
+++ run.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.secret_key = "some_secret"
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get("PORT")),
+            debug=True)
+            
